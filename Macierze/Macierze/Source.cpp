4b337f92cabf4babc91bfa4ecd0039537edc6957
DawJas <129426996+DawJas@users.noreply.github.com>
2023-04-12 20:47:46 +0200

    g
    gitignore

--- Macierze/Macierze/Source.cpp
+++ Macierze/Macierze/Source.cpp
@@ -3,21 +3,21 @@
 using namespace std;
 
 int main() {
-	init(3);
-	const int N = 933;
-	Matrix A = Matrix(5, 5, 5.0);
-	//A.addDiagonal(12,MAIN_DIAGONAL);
-	//A.addDiagonal(-1.0,-1);
-	//A.addDiagonal(-1.0,-2);
-	//A.addDiagonal(-1.0,1);
-	//A.addDiagonal(-1.0,2);
+	//init(3);
+	const int N = 2000;
+	Matrix A = Matrix(N, N, 0.0);
+	A.addDiagonal(12,MAIN_DIAGONAL);
+	A.addDiagonal(-1.0,-1);
+	A.addDiagonal(-1.0,-2);
+	A.addDiagonal(-1.0,1);
+	A.addDiagonal(-1.0,2);
 	double* b = bVector(N);
 
-//	GaussSeidl(A, b);
-	//Jacobi(A, b);
+	GaussSeidl(A, b);
+	Jacobi(A, b);
 	LUFactorization(A, b);
-
-
 	A.freeNumbers();
+
+	
 	return 0;
 }
